c8009481df27d3940ad0e44d6b3e1b8fcbcd09ce
Esther <esther191299@gmail.com>
2019-09-15 20:59:08 +0200

    Kelvin To Celcius

--- src/temperatureConversionProgramCFK/TemperatureConversionProgram.java
+++ src/temperatureConversionProgramCFK/TemperatureConversionProgram.java
@@ -29,13 +29,20 @@ public class TemperatureConversionProgram {
 		System.out.println("Voer de waarde temperatuur in in cijfers");
 		inputValue = playerInput.nextFloat();
 		
+		
 		/*TO-CELSIUS-COMVERSION POINT*/
 		//Here We Convert All Cases To Celsius// /*so later on we can convert a celsuis value to the prevered type*/
-		/* We don't have an explicit conversion from Kelvin to Celsius, BUT WE DO KNOW how to convert Kelvin to Fahrenheit, AND THEN how to convert Fahrenheit to Celsius. So we can get away with doing something like the following on line 38*/
+		/* We don't have an explicit conversion from Kelvin to Celsius, BUT WE DO KNOW how to convert Kelvin to Fahrenheit, AND THEN how to convert Fahrenheit to Celsius. So we can get away with doing something like the following on line 45*/
+		/* To be quite honest: Yes we know we are converting Kelvin to Celsuis BUT.. The math looks very daunthing. This is an example of Magic Code. Nobody really knows whats happening. Only the computer I guess. I hope in the next commit to improve this*/
+		
 		switch(inputType) {
-		case 'F': inputValue = ((inputValue-32.0f)*5.0f)/9.0f;
-		case 'C': break;
+		case 'F':
+			inputValue = ((inputValue-32.0f)*5.0f)/9.0f;
+			break;
+		case 'C':
+			break;
 		case 'K':
+			inputValue = inputValue = ((((((inputValue-273.15f)*9.0f)/5.0f)+32.0f) - 32.0f)*5.0f)/9.0f;
 			default:
 			System.exit(1);
 		}
